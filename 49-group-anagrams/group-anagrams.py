bf563c26362fcf99a4adc5151877c54718580acd
Vivek Malapaka <malapakavivek628@gmail.com>
2026-07-18 08:29:31 +0530

    Time: 12 ms (57.08%) | Memory: 21.6 MB (97.53%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         count = {}
-        ans =[]
-        for i in range (len(strs)):
-            sort = sorted(strs[i])
-            a = "".join(sort)
-            count[a] = count.get(a,[])+[strs[i]]
-        for v in count.values():
+        ans = []
+        for i in strs:
+            sorted_i = sorted(i)
+            string = "".join(sorted_i)
+            count[string] = count.get(string,[])+[i]
+        
+        for k,v in count.items():
             ans.append(v)
         return ans
