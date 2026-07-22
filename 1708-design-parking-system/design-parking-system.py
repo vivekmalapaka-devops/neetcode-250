73f11cec41e274a462d2a3cad83dae6d4c623185
Vivek Malapaka <malapakavivek628@gmail.com>
2026-07-22 23:21:32 +0530

    Time: 5 ms (93.83%) | Memory: 19.7 MB (59.23%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -4,21 +4,10 @@ class ParkingSystem:
         self.carSize = list((big,medium,small))
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.carSize[0]!=0:
-                self.carSize[0] = self.carSize[0]-1
-                return True
-            return False
-        if carType == 2:
-            if self.carSize[1]!=0:
-                self.carSize[1] = self.carSize[1]-1
-                return True
-            return False
-        if carType == 3:
-            if self.carSize[-1]!=0:
-                self.carSize[-1] = self.carSize[-1]-1
-                return True
-            return False  
+        if self.carSize[carType-1]!=0:
+            self.carSize[carType-1] = self.carSize[carType-1]-1
+            return True
+        return False
             
 
 # Your ParkingSystem object will be instantiated and called as such:
