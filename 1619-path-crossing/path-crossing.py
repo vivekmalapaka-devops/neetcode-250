1e332ed55123d2c0826d9dc3d9d746e20f72d737
Vivek Malapaka <malapakavivek628@gmail.com>
2026-08-13 23:11:06 +0530

    Time: 4 ms (7.07%) | Memory: 19.2 MB (65.77%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -1,29 +1,47 @@
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        seen = [[0,0]]
+        seen = [[0, 0]]
+
+        def is_seen(curr):
+            if curr in seen:
+                return True
+            return False
+
         for i in path:
             if i == "N":
                 last = seen[-1]
-                curr = [last[0],last[-1]+1]
-                if curr in seen:
+                curr = [last[0], last[-1] + 1]
+
+                if is_seen(curr):
                     return True
+
                 seen.append(curr)
+
             elif i == "E":
                 last = seen[-1]
-                curr = [last[0]+1,last[-1]]
-                if curr in seen:
+                curr = [last[0] + 1, last[-1]]
+
+                if is_seen(curr):
                     return True
+
                 seen.append(curr)
+
             elif i == "S":
                 last = seen[-1]
-                curr = [last[0],last[-1]-1]
-                if curr in seen:
-                    return True     
+                curr = [last[0], last[-1] - 1]
+
+                if is_seen(curr):
+                    return True
+
                 seen.append(curr)
+
             elif i == "W":
                 last = seen[-1]
-                curr = [last[0]-1,last[-1]]
-                if curr in seen:
+                curr = [last[0] - 1, last[-1]]
+
+                if is_seen(curr):
                     return True
+
                 seen.append(curr)
+
         return False
